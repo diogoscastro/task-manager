18e7b49524dd644d30ac7a0f674eb0b57742ffad
diogoscatro <diogocas89@gmail.com>
2025-10-23 19:18:30 -0300

    feat: suitability to work in render

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ connectToDatabase();
 
 app.use("/tasks", TaskRouter);
 
-app.listen(8000, () => console.log("Listening on port 8000!"));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
