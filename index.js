4913a7f2c171f0222fa4aa62ba174fe6df0dce36
diogoscatro <diogocas89@gmail.com>
2025-10-23 21:42:48 -0300

    feat: added app.get to test API

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ connectToDatabase();
 
 app.use("/tasks", TaskRouter);
 
+app.get("/", (req, res) => {
+    res.send("API está funcionando!");
+});
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
